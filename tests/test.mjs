71490a3372fd7538d4b94bca4a79c8966fc69d2d
Santosh Rajan <santrajan@gmail.com>
2018-03-04 11:33:10 +0530

    Makes App Server

--- tests/test.mjs
+++ tests/test.mjs
@@ -1,27 +1,22 @@
-import childProcess from 'child_process'
 import fetch from 'node-fetch'
 import test from 'tape'
+import App from '../lib/server'
 
-const spawn = childProcess.spawn
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000})
-const child = spawn('node', ['lib/server.js'], {env})
+// Start App Server
+App.listen(5000)
 
-test('responds to requests', (t) => {
+test('responds to requests', async (t) => {
   t.plan(3)
-  // Wait until the server is ready
-  child.stdout.on('data', async _ => {
-    let res, text, error
-    try {
-      res = await fetch('http://127.0.0.1:5000')
-      text = await res.text()
-      // stop the server
-      child.kill()
-    } catch (e) {
-      error = e
-    }
-    t.false(error)
-    t.equal(res.status, 200)
-    t.equal(text, 'hello world')
-  })
+  let res, text, error
+  try {
+    res = await fetch('http://127.0.0.1:5000')
+    text = await res.text()
+  } catch (e) {
+    error = e
+  }
+  t.false(error)
+  t.equal(res.status, 200)
+  t.equal(text, 'hello world')
+  // Shutdoqn App Server
+  App.close()
 })
