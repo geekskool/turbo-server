49844b8ad188e50e5dea5e53fdd377f4bc52a486
Santosh Rajan <santrajan@gmail.com>
2018-03-04 10:13:11 +0530

    test.js now uses async/await

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 const { spawn } = require('child_process')
-const request = require('request')
+// const request = require('request')
+const fetch = require('node-fetch')
 const test = require('tape')
 
 // Start the app
@@ -10,19 +11,18 @@ test('responds to requests', (t) => {
   t.plan(3)
 
   // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    console.log('here')
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
+  child.stdout.on('data', async _ => {
+    let error
+    try {
+      const res = await fetch('http://127.0.0.1:5000')
+      const text = await res.text()
       // stop the server
       child.kill()
-
-      // No error
-      t.false(error)
-      // Successful response
-      t.equal(response.statusCode, 200)
-      // Assert content checks
-      t.notEqual(body.indexOf('hello world'), -1)
-    })
+      t.equal(res.status, 200)
+      t.equal(text, 'hello world')
+    } catch (e) {
+      error = e
+    }
+    t.false(error)
   })
 })
