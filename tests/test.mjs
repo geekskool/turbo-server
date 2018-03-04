0b674a8f9aa9b7ade1e669a48d505e9ca8a002da
Santosh Rajan <santrajan@gmail.com>
2018-03-04 11:01:25 +0530

    uses experimental modules

--- tests/test.mjs
+++ tests/test.mjs
@@ -1,28 +1,27 @@
-const { spawn } = require('child_process')
-// const request = require('request')
-const fetch = require('node-fetch')
-const test = require('tape')
+import childProcess from 'child_process'
+import fetch from 'node-fetch'
+import test from 'tape'
 
+const spawn = childProcess.spawn
 // Start the app
 const env = Object.assign({}, process.env, {PORT: 5000})
 const child = spawn('node', ['lib/server.js'], {env})
 
 test('responds to requests', (t) => {
   t.plan(3)
-
   // Wait until the server is ready
   child.stdout.on('data', async _ => {
-    let error
+    let res, text, error
     try {
-      const res = await fetch('http://127.0.0.1:5000')
-      const text = await res.text()
+      res = await fetch('http://127.0.0.1:5000')
+      text = await res.text()
       // stop the server
       child.kill()
-      t.equal(res.status, 200)
-      t.equal(text, 'hello world')
     } catch (e) {
       error = e
     }
     t.false(error)
+    t.equal(res.status, 200)
+    t.equal(text, 'hello world')
   })
 })
