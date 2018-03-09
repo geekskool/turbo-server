ae6d53ad6f2172e1f15f8ddce50b3128ba5c7050
Santosh Rajan <santrajan@gmail.com>
2018-03-09 14:02:32 +0530

    adds router

--- tests/test.mjs
+++ tests/test.mjs
@@ -7,17 +7,10 @@ const app = new App()
 app.listen() // process.env.PORT || 5000
 
 test('responds to requests', async (t) => {
-  t.plan(3)
+  t.plan(9)
   let res, text, error
   try {
     res = await fetch('http://127.0.0.1:5000/aa')
-  /*
-    res = await fetch('http://127.0.0.1:5000', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({hello: 'world'})
-    })
-  */
     text = await res.text()
   } catch (e) {
     error = e
@@ -25,6 +18,28 @@ test('responds to requests', async (t) => {
   t.false(error)
   t.equal(res.status, 404)
   t.equal(text, 'Not Found')
+  try {
+    res = await fetch('http://127.0.0.1:5000')
+    text = await res.text()
+  } catch (e) {
+    error = e
+  }
+  t.false(error)
+  t.equal(res.status, 200)
+  t.equal(text, '<h1>hello world</h1>\n')
+  try {
+    res = await fetch('http://127.0.0.1:5000', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({hello: 'world'})
+    })
+    text = await res.text()
+  } catch (e) {
+    error = e
+  }
+  t.false(error)
+  t.equal(res.status, 200)
+  t.equal(text, '<h1>hello world</h1>\n')
   // Shutdown App Server
   app.close()
 })
