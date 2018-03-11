74e104e88b35d30dd43e3d4311516eefb1a2792e
Santosh Rajan <santrajan@gmail.com>
2018-03-11 15:34:31 +0530

    adds Router

--- tests/test.mjs
+++ tests/test.mjs
@@ -1,45 +1,51 @@
 import fetch from 'node-fetch'
 import test from 'tape'
 import App from '../lib/server'
+import Router from '../lib/router'
 
 // Start App Server
 const app = new App()
+const router = new Router('/')
+router.post('/', function () {
+  this.res.send(this.body)
+})
+app.addRouter(router)
 app.listen() // process.env.PORT || 5000
 
 test('responds to requests', async (t) => {
   t.plan(9)
-  let res, text, error
+  let res, data, error
   try {
     res = await fetch('http://127.0.0.1:5000/aa')
-    text = await res.text()
+    data = await res.text()
   } catch (e) {
     error = e
   }
   t.false(error)
   t.equal(res.status, 404)
-  t.equal(text, 'Not Found')
+  t.equal(data, 'Not Found')
   try {
     res = await fetch('http://127.0.0.1:5000')
-    text = await res.text()
+    data = await res.text()
   } catch (e) {
     error = e
   }
   t.false(error)
   t.equal(res.status, 200)
-  t.equal(text, '<h1>hello world</h1>\n')
+  t.equal(data, '<h1>hello world</h1>\n')
   try {
     res = await fetch('http://127.0.0.1:5000', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({hello: 'world'})
     })
-    text = await res.text()
+    data = await res.json()
   } catch (e) {
     error = e
   }
   t.false(error)
   t.equal(res.status, 200)
-  t.equal(text, '<h1>hello world</h1>\n')
+  t.deepEqual(data, {hello: 'world'})
   // Shutdown App Server
   app.close()
 })
