5d78265e2272e79fb37c8983d46aaf3f8deabd65
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-03-22 13:41:47 +0530

    Add session example

--- tests/app.mjs
+++ tests/app.mjs
@@ -5,16 +5,17 @@ const app = new App()
 const router = new App.Router('/')
 
 router.get('/new', function () {
-  this.res.send(this.session.ole)
-})
-
-router.get('/check', function () {
   console.log('app', this)
   this.session.ole = 'yola'
   this.res.send(this.session)
 })
 
+router.get('/check', function () {
+  this.res.send(this.session.ole)
+})
+
 router.get('/delete', function () {
+  delete this.session.ole
   this.res.send('')
 })
 
