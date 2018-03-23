4bd2d0c08bcc2ce9497f410fb23d13bb31fbbc79
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-03-23 08:49:26 +0530

    Make Session a class

--- tests/app.mjs
+++ tests/app.mjs
@@ -4,19 +4,24 @@ import App from '../lib/server'
 const app = new App()
 const router = new App.Router('/')
 
+const html = '<a href="/new">new</a><br><a href="/check">check</a><br><a href="/delete">delete</a>'
+
 router.get('/new', function () {
   console.log('app', this)
-  this.session.ole = 'yola'
-  this.res.send(this.session)
+  this.session.set('yo', 'lo')
+  this.res.send(html)
 })
 
 router.get('/check', function () {
-  this.res.send(this.session.ole)
+  console.log('app', this)
+  this.session.get('yo')
+  this.res.send(html)
 })
 
 router.get('/delete', function () {
-  delete this.session.ole
-  this.res.send('')
+  console.log('app', this)
+  this.session.set('yo')
+  this.res.send(html)
 })
 
 app.addRouter(router)
